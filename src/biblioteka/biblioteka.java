1816fe61e2b4cc5cb6958de1fef90bfdee20336d
Thekgiga <Thekgiga@10.20.30.236>
2015-03-17 12:53:14 +0100

    Dodata logicka kontrola

--- src/biblioteka/biblioteka.java
+++ src/biblioteka/biblioteka.java
@@ -11,13 +11,20 @@ public class biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga == null)
+			throw new RuntimeException("Knjiga ne sme biti null");
+		if(knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga je vec u biblioteci");
+		
 		knjige.add(knjiga);
-		
-		
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		
+		if(!knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga ne postoji u biblioteci");
+	
 		knjige.remove(knjiga);
 	}
 
@@ -26,6 +33,9 @@ public class biblioteka implements BibliotekaInterfejs {
 			String izdavac, Autor autor) {
 		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		if(naslov==null && ISBN <=0 && izdavac==null && autor==null)
+			throw new RuntimeException("Morate uneti neki kriterijum");
 		
 		if(ISBN > 0)
 			for(int i =0; i<knjige.size();i++){
